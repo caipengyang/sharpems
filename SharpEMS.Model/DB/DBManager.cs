ac9e502c236777c349be13ce43d3a1bc0cfa62ee
yangang <caipengy@mail3.sysu.edu.cn>
2013-11-10 06:08:19 +0800

    ci

---
+++ SharpEMS.Model/DB/DBManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SqliteORM;
+
+
+namespace SharpEMS.Model.DB
+{
+    class DBManager
+    {
+        static void Main()
+        {
+            DbConnection.Initialise("data source=database3.db");
+        }
+    }
+}
