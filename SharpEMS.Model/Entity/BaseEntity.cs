33b728a67619d949b59280653c26bd7c0c793367
yangang <caipengy@mail3.sysu.edu.cn>
2013-11-10 12:57:38 +0800

    ci

---
+++ SharpEMS.Model/Entity/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpEMS.Model.Entity
+{
+    public class BaseEntity
+    {
+        protected string ClassName { get; set; }
+
+        protected BaseEntity(string className)
+        {
+            this.ClassName = className;
+        }
+    }
+}
---
+++ SharpEMS.Model/Entity/UserEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpEMS.Model.Entity
+{
+    public class UserEntry : BaseEntity
+    {
+        public int Id { get; set; }
+        public string LoginName { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public int Sex { get; set; }
+        public string Mobile { get; set; }
+        public string Address { get; set; }
+        public UserEntry():base("UserEntity"){}
+    }
+}
