4dcc41b57bcb05a79d799a06790742c48e97a6fc
yangang <caipengy@mail3.sysu.edu.cn>
2013-11-10 23:06:13 +0800

    ci

---
+++ SharpEMS.Net/Add.cs
@@ -0,0 +1,19 @@
+﻿using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Command;
+using SuperSocket.SocketBase.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpEMS.Net
+{
+    public class Add : CommandBase<SharpSession, StringRequestInfo>
+    {
+        public override void ExecuteCommand(SharpSession session, StringRequestInfo requestInfo)
+        {
+            session.Send(requestInfo.Parameters.Select(p => Convert.ToInt32(p)).Sum().ToString());
+        }
+    }
+}
---
+++ SharpEMS.Net/Class1.cs
@@ -0,0 +1,46 @@
+﻿using SuperSocket.SocketBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpEMS.Net
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            var appServer = new SharpServer();
+            if(!appServer.Setup(2012))
+            {
+                Console.WriteLine("setup server failed.");
+                Console.ReadKey();
+                return;
+            }
+
+            appServer.NewSessionConnected += appServer_NewSessionConnected;
+
+            //appServer.NewRequestReceived += appServer_NewRequestReceived;
+
+            if(!appServer.Start())
+            {
+                Console.WriteLine("start server failed.");
+                Console.ReadKey();
+                return;
+            }
+
+            while(Console.ReadLine().ToUpper() != "EXIT")
+            {
+
+            }
+            Console.WriteLine("stop server.");
+            appServer.Stop();
+        }
+
+        static void appServer_NewSessionConnected(SharpSession session)
+        {
+            Console.WriteLine("client connected.");
+        }
+    }
+}
