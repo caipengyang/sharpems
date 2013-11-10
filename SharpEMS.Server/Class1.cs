27f15ba3944e80cb078feefae77af7b7c74db83b
yangang <caipengy@mail3.sysu.edu.cn>
2013-11-10 23:04:23 +0800

    ci

--- SharpEMS.Server/Class1.cs
+++ SharpEMS.Server/Class1.cs
@@ -8,5 +8,9 @@ namespace SharpEMS.Server
 {
     public class Class1
     {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to start the server.");
+        }
     }
 }
